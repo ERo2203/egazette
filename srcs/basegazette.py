8bb7735ec3a7440317bf6a479ffc7039bcd95190
ERo2203 <144051684+ERo2203@users.noreply.github.com>
2024-09-25 00:46:17 +0530

    "Updated import statement for Retry from requests.packages.urllib3.util to urllib3.util"

--- srcs/basegazette.py
+++ srcs/basegazette.py
@@ -5,7 +5,7 @@ import os
 import time
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util import Retry
 
 from ..utils import utils
 
